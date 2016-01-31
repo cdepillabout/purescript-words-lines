3afdc5de0aed88f339f8d2e77f0dd27e40cc6b5a
(cdep)illabout <cdep.illabout@gmail.com>
2016-02-01 01:12:56 +0900

    Added words function.

--- src/Data/Array/WordsLines.purs
+++ src/Data/Array/WordsLines.purs
@@ -3,7 +3,11 @@ module Data.Array.WordsLines where
 
 import Prelude
 
+import Data.Array as Array
+import Data.Char.Unicode
 import Data.Foldable
+import Data.Maybe
+import Data.String as String
 
 unwords :: Array String -> String
 unwords = foldl f ""
@@ -17,3 +21,17 @@ unlines = foldl f ""
   where
     f :: String -> String -> String
     f acc a = acc <> a <> "\n"
+
+words :: String -> Array String
+words = map String.fromCharArray <<< go <<< String.toCharArray
+  where
+    go :: Array Char -> Array (Array Char)
+    go s =
+        case Array.uncons $ Array.dropWhile isSpace s of
+            Nothing -> []
+            Just { head: head, tail: tail } ->
+                let xxx = break isSpace (head `Array.cons` tail)
+                in xxx.init `Array.cons` go xxx.rest
+
+break :: forall a . (a -> Boolean) -> Array a -> { init :: (Array a), rest :: (Array a) }
+break p = Array.span (not <<< p)
